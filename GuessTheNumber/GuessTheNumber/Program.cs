eaf3d92f1f476edf46d327bf02d9e6d8f9accf2f
Ed <e_ruic@hotmail.com>
2021-09-11 17:25:43 -0400

    adding new project to repo

---
+++ GuessTheNumber/GuessTheNumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace GuessTheNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random number = new Random();
+            int secretNumber = number.Next(1, 100);
+            int guess = 0;
+
+            while (guess != secretNumber)
+            {
+                Console.WriteLine("What is your guess?");
+                string guessText = Console.ReadLine();
+
+                guess = int.Parse(guessText);
+                if (guess == secretNumber)
+                {
+                    Console.WriteLine("You guess correctly!");
+                }
+                else if (guess > secretNumber)
+                {
+                    Console.WriteLine("You guessed too high...");
+                }
+                else if (guess < secretNumber)
+                {
+                    Console.WriteLine("You guessed too low...");
+                }
+            }
+        }
+    }
+}
